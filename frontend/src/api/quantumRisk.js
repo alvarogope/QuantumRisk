d086d743123aa42e1e9d5436ca9da1b6407c78d1
Hxrxm <alvarogope99@gmail.com>
2026-06-26 16:18:00 +0100

    update API base URL to Vercel backend

--- frontend/src/api/quantumRisk.js
+++ frontend/src/api/quantumRisk.js
@@ -1,14 +1,6 @@
 import axios from 'axios'
 
-// What is axios?
-
-const BASE_URL = 'http://localhost:8000'
-
-// How does js know what is fetchMarketData?
-
-// What is async?
-
-// What is . in js?
+const BASE_URL = 'https://quantum-risk.vercel.app'
 
 export const fetchMarketData = async (ticker) => {
     const response = await axios.get(`${BASE_URL}/market-data/${ticker}`)
@@ -19,8 +11,6 @@ export const priceOption = async (params) => {
     const response = await axios.post(`${BASE_URL}/price-option`, params)
     return response.data
 }
-
-// What is =>?
 
 export const portfolioVar = async (params) => {
     const response = await axios.post(`${BASE_URL}/portfolio-var`, params)
