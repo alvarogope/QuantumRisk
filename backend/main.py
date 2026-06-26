c0acda22963629b2560fe6d2064dfcf8ab15293a
Hxrxm <alvarogope99@gmail.com>
2026-06-26 15:56:03 +0100

    add requirements.txt and update CORS for deployment

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ app = FastAPI (
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Vite's default React port
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"]
 )
